00babc3c10c6a064acef25a14b38f180662492b1
Nawar Ali <nr.ali1912@gmail.com>
2024-10-08 22:20:34 +0200

    Implement_Global_Exception_Handling

--- src/API/EventZen.Api/Program.cs
+++ src/API/EventZen.Api/Program.cs
@@ -1,4 +1,5 @@
 using EventZen.Api.Extensions;
+using EventZen.Api.Middleware;
 using EventZen.Modules.Events.Infrastructure;
 using EventZen.Shared.Application;
 using EventZen.Shared.Infrastructure;
@@ -12,22 +13,26 @@ builder.Host.UseSerilog((context, loggerConfiguration) =>
     loggerConfiguration.ReadFrom.Configuration(context.Configuration);
 });
 
+// Enable global exception handling
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
     options.CustomSchemaIds(t => t.FullName?.Replace("+", "."));
 });
 
+// shared dependencies registration
 builder.Services.AddApplication([
     EventZen.Modules.Events.Application.AssemblyReference.Assembly,
     ]);
-
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Configuration.AddModuleConfiguration(["events"]);
 
+// modules registration
 builder.Services.AddEventModules(builder.Configuration);
-
 
 WebApplication app = builder.Build();
 
@@ -42,5 +47,7 @@ if (app.Environment.IsDevelopment())
 EventsModule.MapEndpoints(app);
 
 app.UseSerilogRequestLogging();
+
+app.UseExceptionHandler();
 
 app.Run();
--- src/Shared/EventZen.Shared.Application/Exceptions/EventZenException.cs
+++ src/Shared/EventZen.Shared.Application/Exceptions/EventZenException.cs
@@ -1,16 +1,12 @@
 ﻿using EventZen.Shared.Domain.Abstractions;
 
 namespace EventZen.Shared.Application.Exceptions;
-public sealed class EventZenException : Exception
+public sealed class EventZenException(
+    string requestName, Error? error = default,
+    Exception? innerException = default)
+    : Exception("Application exception", innerException)
 {
-    public EventZenException(string requestName, Error? error = default, Exception? innerException = default)
-        : base("Application exception", innerException)
-    {
-        RequestName = requestName;
-        Error = error;
-    }
+    public string RequestName { get; } = requestName;
 
-    public string RequestName { get; }
-
-    public Error? Error { get; }
+    public Error? Error { get; } = error;
 }
